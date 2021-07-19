e910170b761a27c995827c833df9bb59beebf560
FelipeAC98 <felipe.alvcor@gmail.com>
2021-07-18 22:32:42 -0300

    Match inicial entre 4mula e spotify

--- matchSpotify4Mula.py
+++ matchSpotify4Mula.py
@@ -1,9 +1,28 @@
+#carregando dados 4mula
 import pandas as pd
-df = pd.read_parquet('4mula_tiny.parquet')
+df = pd.read_parquet('4mula_metadata.parquet')
+
+#conectando a API
+import spotifyAPIConection
+
+access_token='BQCELmVlfPoBgVn5cgT8tOOJzR3uznWawKrY8Oi5qNqdng1NWGzVPYP56njzdkwu34PHWxIYZAE-HooTDHzsBHvVNwoO0KjYvb4DPcw3YZ8QgmQAgCz3fDx_b1XGGWW6qmoVIrVuP_0sqIaR9v494XC_Sq8'
+conexaoSpotify=spotifyAPIConection.spotifyAPIConection(access_token)
 
 #percorrendo todas linhas do df
 for index, row in df.iterrows():
-	musicName=row['music_name']
-	artName=row['art_name']
+	trackName=row['music_name']
+	artistName=row['art_name']
 
+	response, respJson=conexaoSpotify.trackSearch(trackName,artistName)
+	print('Nome buscado: '+ trackName)
+
+	for item in respJson['tracks']['items'] :
+		print('Nome no spotify: '+ item['name'])
+		#print(item['id'])
+
+		if item['name']!=trackName:
+			print('nome diferente do esperado')
+
+		input("Press Enter to continue...")
 	
+	print('\n')
