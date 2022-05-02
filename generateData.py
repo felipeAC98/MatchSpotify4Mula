43c289a25af9713bb005622682ad4feb59b2a07c
FelipeAC98 <felipe.alvcor@gmail.com>
2022-05-01 22:31:24 -0300

    generateData: Insercao de try exception

--- generateData.py
+++ generateData.py
@@ -2,6 +2,7 @@ from classes.spotifyAPI import spotifyData, spotifyAPIConnection
 import sys
 import clientToken
 import csv
+import traceback
 
 trackRequestLimit=50
 fileName='spotifyDataset'
@@ -14,34 +15,40 @@ def main():
 	_spotifyConnection=spotifyAPIConnection(clientToken.CLIENT_ID, clientToken.CLIENT_SECRET)
 	_spotifyData=spotifyData(_spotifyConnection=_spotifyConnection)
 
-	with open(fileName+'.csv', 'a') as csvFile:
+	with open(fileName+'.csv', 'w') as csvFile:
 
 		#Obtendo o nome dos parametros apartir de uma trackID exemplo
 		fieldnames=_spotifyData.get_field_names()
 
 		write = csv.DictWriter(csvFile, delimiter=',', lineterminator='\n',fieldnames=fieldnames)
 
-		for year in range(1980,2023):
+		for year in range(1970,2023):
 
-			numberTracksByYear=defaultNumberTracksByYear*nTracksIncreasePercent
-			nTracksIncreasePercent=nTracksIncreasePercent+0.05 #aumentando em 5% o numero de musicas por ano
+			numberTracksByYear=int(defaultNumberTracksByYear*nTracksIncreasePercent)
+			#nTracksIncreasePercent+=0.025 #aumentando em 2.5% o numero de musicas por ano
 			index=0
 
 			print("Year: "+str(year)+" numberTracksByYear: "+str(numberTracksByYear))
-			
-			while(index<numberTracksByYear):
 
-				index+=trackRequestLimit
+			while(index<numberTracksByYear):
 
 				response, respJson=_spotifyConnection.get_type_by_year(index=index,year=year,limit=trackRequestLimit)
 
-				for track in respJson['tracks']['items']:
+				try:
 
-					trackID=track['id']
+					for track in respJson['tracks']['items']:
 
-					features=_spotifyData.get_track_features(trackID)
+						trackID=track['id']
 
-					write.writerow(features)
+						features=_spotifyData.get_track_features(trackID)
+
+						write.writerow(features)
+
+				except:
+					print(' Erro obtendo track da resposta para o index: '+str(index)+ ' mensagem de erro: '+str(traceback.format_exc()))
+					break
+
+				index+=trackRequestLimit
 
 if __name__ == '__main__':
     sys.exit(main())
